b29ac17131c56548823aac9b53a9bac3d226100f
m26416141 <m26416141@john.petra.ac.id>
2021-03-16 16:24:17 +0800

    v4

--- src/app/globar-var.service.ts
+++ src/app/globar-var.service.ts
@@ -5,5 +5,34 @@ import { Injectable } from '@angular/core';
 })
 export class GlobarVarService {
 
-  constructor() { }
+  private judul: String[]=[];
+  private isinote: String[]=[];
+  private tglnote: String[]=[];
+  private jumlahnote: number[]=[];
+  private count: number = 0;
+
+  constructor() {}
+
+  public getjudul() {
+    return this.judul
+  }
+  public getisinote() {
+    return this.isinote
+  }
+  public gettglnote() {
+    return this.tglnote
+  }
+  public getjumlahnote() {
+    return this.jumlahnote
+  }
+  public getcount() {
+    return this.count
+  }
+  public insertnote(judul, isi, tgl){
+    this.judul.push(judul)
+    this.isinote.push(isi)
+    this.tglnote.push(tgl)
+    this.jumlahnote.push(this.count)
+    this.count++
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { GlobarVarService } from '../globar-var.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  inputjudul = ""; inputisi = ""; inputtgl = "";
+  publicjudul; publicisi; publictgl; publicjumlahnote; publiccount;
+
+  constructor(private router: Router, public globalvar: GlobarVarService) {
+    this.publicjudul = this.globalvar.getjudul()
+    this.publicisi = this.globalvar.getisinote()
+    this.publictgl = this.globalvar.gettglnote()
+    this.publicjumlahnote = this.globalvar.getjumlahnote()
+  }
+
+  savenote() {
+    this.globalvar.insertnote(this.inputjudul,this.inputisi,this.inputtgl)
+    this.publicjudul = this.globalvar.getjudul()
+    this.publicisi = this.globalvar.getisinote()
+    this.publictgl = this.globalvar.gettglnote()
+    this.publicjumlahnote = this.globalvar.getjumlahnote()
+    this.publiccount = this.globalvar.getcount()
+    console.log(this.publicjudul)
+  }
 
 }
